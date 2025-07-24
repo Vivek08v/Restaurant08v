cfad957f213b3b90c2af6a4f00b45c2a2eec9c8e
Vivek08v <vivekkumar08v@gmail.com>
2025-07-24 18:56:53 +0530

    initial files and dependency setup

---
+++ src/test/java/com/vivek08v/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek08v.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
