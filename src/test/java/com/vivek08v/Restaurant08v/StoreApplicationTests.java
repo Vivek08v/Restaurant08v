15493667eeb741c79bd8081fdd936e72a7f96c88
Vivek08v <vivekkumar08v@gmail.com>
2025-07-24 20:17:38 +0530

    name change

---
+++ src/test/java/com/vivek08v/Restaurant08v/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek08v.Restaurant08v;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
