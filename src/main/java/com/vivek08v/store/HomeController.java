d2bf4f7be504033745d38ebf084b404ee58612ec
Vivek08v <vivekkumar08v@gmail.com>
2025-07-24 19:53:37 +0530

    controller and restController

---
+++ src/main/java/com/vivek08v/store/HomeController.java
@@ -0,0 +1,20 @@
+package com.vivek08v.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("appName: " + appName);
+        return "vivek";
+    }
+    
+}
